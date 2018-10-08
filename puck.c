34b82c663fa2f3bead5c702a549b85c152d78342
dalyIsaac <isaac.daly@outlook.com>
2018-10-08 17:14:38 +1300

    Added documentation for puck_update_value

--- puck.c
+++ puck.c
@@ -47,6 +47,10 @@ void puck_init(void)
     puck_update_display();
 }
 
+/**
+ * @brief Updates the value of a puck, following user input with the navswitch
+ * @param change The change to the puck's position
+ */
 void puck_update_value(NavMovement change)
 {
     if (puck.new_bottom + change >= 0 && puck.new_top + change < LEDMAT_ROWS_NUM)
